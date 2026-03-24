3365811fb111c0e07e4d65015a72c085a9cac948
20JANO1-DidierAlvarez <j.alvarez.5cdb@gmail.com>
2026-03-24 09:52:05 -0600

    Add files via upload

---
+++ Ejerciciodel17demarzo en SEASHARP.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese un numero: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+        if (numero >= 0)
+        {
+            Console.WriteLine("El numero es positivo");
+        }
+        else
+        {
+            Console.WriteLine("El numero es negativo");
+        }
+
+    }
+}
